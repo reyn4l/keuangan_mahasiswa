1e09e5d2a38c08e2c4d5ce98a668438900117e87
reynal <kurniawanreynaldi8@gmail.com>
2024-07-04 12:00:30 +0700

    init

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: SafeArea(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // dashboard total pemasukan dan pengeluaran
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        child: Icon(Icons.download, color: Colors.green),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(8)),
+                      ),
+                      SizedBox(width: 15),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Pemasukan",
+                            style: GoogleFonts.montserrat(
+                                color: Colors.white, fontSize: 12),
+                          ),
+                          SizedBox(height: 10),
+                          Text("Rp.1.000.000",
+                              style: GoogleFonts.montserrat(
+                                  color: Colors.white, fontSize: 14))
+                        ],
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        child: Icon(Icons.upload, color: Colors.red),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(8)),
+                      ),
+                      SizedBox(width: 15),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Pengeluaran",
+                            style: GoogleFonts.montserrat(
+                                color: Colors.white, fontSize: 12),
+                          ),
+                          SizedBox(height: 10),
+                          Text("Rp.1.000.000",
+                              style: GoogleFonts.montserrat(
+                                  color: Colors.white, fontSize: 14))
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+              width: double.infinity,
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 25, 45, 61),
+                borderRadius: BorderRadius.circular(16),
+              ),
+            ),
+          ),
+
+          //text transaksi
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Text(
+              "Transaksi",
+              style: GoogleFonts.montserrat(
+                  fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ),
+
+          // list transaksi
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Card(
+              elevation: 10,
+              child: ListTile(
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.delete),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Icon(Icons.edit)
+                  ],
+                ),
+                title: Text("Rp. 20.000"),
+                subtitle: Text("Makan Siang"),
+                leading: Container(
+                  child: Icon(Icons.upload, color: Colors.red),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8)),
+                ),
+              ),
+            ),
+          ),
+           Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Card(
+              elevation: 10,
+              child: ListTile(
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.delete),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Icon(Icons.edit)
+                  ],
+                ),
+                title: Text("Rp. 500.000"),
+                subtitle: Text("Saku Bulanan"),
+                leading: Container(
+                  child: Icon(Icons.upload, color: Colors.green),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8)),
+                ),
+              ),
+            ),
+          )
+        ],
+      )),
+    );
+  }
+}
